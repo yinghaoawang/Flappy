967f8bfc6010b37c9e668c821a569d59057e7ec9
yinghaoawang <yinghao.a.wang@gmail.com>
2018-05-29 15:59:59 -0700

    bird constants and better gravity

--- bird.js
+++ bird.js
@@ -1,21 +1,31 @@
+var BIRDWIDTH = 20;
+var BIRDHEIGHT = 20;
+var BIRDXV = 2;
+var GRAVITY = .5;
+var BIRDMAXYV = 4;
+var BIRDJUMPV = -10;
+
 class Bird extends PIXI.Sprite {
     constructor(x, y) {
         super(new PIXI.Texture.fromImage('bird.png'));
         this.x = x;
         this.y = y;
-        this.width = 30;
-        this.height = 30;
-        this.xv = 2;
+        this.width = BIRDWIDTH;
+        this.height = BIRDHEIGHT;
+        this.xv = BIRDXV;
         this.yv = 0;
     }
     step() {
         this.x += this.xv;
-        if (this.yv <= 4)
-            this.yv += .1;
+        if (this.yv <= BIRDMAXYV) {
+            this.yv += GRAVITY;
+        } else {
+            this.yv = BIRDMAXYV;
+        }
         this.y += this.yv;
     }
     jump() {
         console.log("j");
-        this.yv = -4;
+        this.yv = BIRDJUMPV;
     }
 }
