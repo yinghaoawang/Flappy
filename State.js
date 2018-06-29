0eb2e2bb8e381e172d7e21d14b112437f8efed55
yinghaoawang <yinghao.a.wang@gmail.com>
2018-06-28 20:15:14 -0700

    game transferred to game state object

--- State.js
+++ State.js
@@ -3,14 +3,14 @@ class State {
         if (this.constructor === State) {
             throw new TypeError('Abstract class cannot be instantiated directly.'); 
         }
-        if (this.on_enter() === undefined) {
-            throw new TypeError('Class missing method from extended abstract class.');
-        }
-        if (this.on_exit() === undefined) {
-            throw new TypeError('Class missing method from extended abstract class.');
-        }
-        if (this.update() === undefined) {
-            throw new TypeError('Class missing method from extended abstract class.');
-        }
+    }
+    on_enter() {
+        throw new TypeError('Class missing method from extended abstract class.');
+    }
+    on_exit() {
+        throw new TypeError('Class missing method from extended abstract class.');
+    }
+    update() {
+        throw new TypeError('Class missing method from extended abstract class.');
     }
 }
